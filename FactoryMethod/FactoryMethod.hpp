68724f78672dbeb4dc1387458f90dbfbeba0a5d9
Fingsinz <fingsinz@outlook.com>
2024-07-20 16:04:31 +0800

    added 工厂方法模式

---
+++ FactoryMethod/FactoryMethod.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+
+class Product {
+public:
+    Product() = default;
+    virtual ~Product() {};
+
+    virtual void use() = 0;
+};
+
+class ConcreteProductA : public Product {
+public:
+    virtual ~ConcreteProductA() {}
+
+    virtual void use() {
+        std::cout << "use ConcreteProductA\n";
+    }
+};
+
+class ConcreteProductB : public Product {
+public:
+    virtual ~ConcreteProductB() {};
+
+    virtual void use() {
+        std::cout << "use ConcreteProductB\n";
+    }
+};
+
+class Factory {
+public:
+    Factory() = default;
+    virtual ~Factory() {};
+
+    virtual Product *createProduct() = 0;
+};
+
+class ConcreteFactoryA : public Factory {
+public:
+    ConcreteFactoryA() = default;
+    virtual ~ConcreteFactoryA() {}
+
+    virtual Product *createProduct() {
+        return new ConcreteProductA();
+    }
+};
+
+class ConcreteFactoryB : public Factory {
+public:
+    ConcreteFactoryB() = default;
+    virtual ~ConcreteFactoryB() {};
+
+    virtual Product *createProduct() {
+        return new ConcreteProductB();
+    }
+};
--- FactoryMethod/Product.cpp
+++
@@ -1,4 +0,0 @@
-#include "Product.h"
-
-Product::~Product()
-{}
