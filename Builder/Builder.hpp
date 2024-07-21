1153179e18f976a47d16d01c05d662837e37882d
Fingsinz <fingsinz@outlook.com>
2024-07-21 19:22:12 +0800

    updated 格式修正

--- Builder/Builder.hpp
+++ Builder/Builder.hpp
@@ -4,61 +4,61 @@
 
 class Product {
 public:
-    void setPart1(const std::string& part1) {
-        this->part1 = part1;
-    }
+	void setPart1(const std::string &part1) {
+		this->part1 = part1;
+	}
 
-    void setPart2(const std::string& part2) {
-        this->part2 = part2;
-    }
+	void setPart2(const std::string &part2) {
+		this->part2 = part2;
+	}
 
-    std::string getProduct() const {
-        return part1 + " " + part2;
-    }
+	std::string getProduct() const {
+		return part1 + " " + part2;
+	}
 
 private:
-    std::string part1;
-    std::string part2;
+	std::string part1;
+	std::string part2;
 };
 
 class Builder {
 public:
-    virtual ~Builder() = default;
-    virtual void buildPart1() = 0;
-    virtual void buildPart2() = 0;
-    virtual Product* getProduct() = 0;
+	virtual ~Builder() = default;
+	virtual void buildPart1() = 0;
+	virtual void buildPart2() = 0;
+	virtual Product *getProduct() = 0;
 };
 
 class ConcreteBuilder : public Builder {
 public:
-    ConcreteBuilder() {
-        product = new Product();
-    }
+	ConcreteBuilder() {
+		product = new Product();
+	}
 
-    ~ConcreteBuilder() {
-        delete product;
-    }
+	~ConcreteBuilder() {
+		delete product;
+	}
 
-    void buildPart1() override {
-        product->setPart1("Part1");
-    }
+	void buildPart1() override {
+		product->setPart1("Part1");
+	}
 
-    void buildPart2() override {
-        product->setPart2("Part2");
-    }
+	void buildPart2() override {
+		product->setPart2("Part2");
+	}
 
-    Product* getProduct() override {
-        return product;
-    }
+	Product *getProduct() override {
+		return product;
+	}
 
 private:
-    Product* product = nullptr;
+	Product *product = nullptr;
 };
 
 class Director {
 public:
-    void construct(Builder* builder) {
-        builder->buildPart1();
-        builder->buildPart2();
-    }
+	void construct(Builder *builder) {
+		builder->buildPart1();
+		builder->buildPart2();
+	}
 };
