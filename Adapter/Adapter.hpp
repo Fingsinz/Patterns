c818475e1d89ab18d499996f080d0b958bd80b68
Fingsinz <fingsinz@outlook.com>
2024-07-30 23:22:58 +0800

    modified 适配器模式：修改代码，添加注释

--- Adapter/Adapter.hpp
+++ Adapter/Adapter.hpp
@@ -1,30 +1,38 @@
 #pragma once
 
+/*
+ * Author: Fingsinz
+ * Date: 2024.07.24
+ *
+ * 适配器模式：将一个类的接口转换成客户希望的另一个接口，充当两个不同接口的桥梁，让不兼容的类一起工作。
+ */
+
 #include <iostream>
 
 // 目标接口
 class Target {
 public:
-	virtual void request() = 0;
+    virtual void request() = 0;
 };
 
 // 被适配者
 class Adaptee {
 public:
-	void specificRequest() {
-		std::cout << "specificRequest" << std::endl;
-	}
+    void specificRequest() {
+        std::cout << "specificRequest" << std::endl;
+    }
 };
 
 // 适配器类
 class Adapter : public Target {
 public:
-	Adapter(Adaptee *adaptee) {
-		m_adaptee = adaptee;
-	}
-	void request() override {
-		m_adaptee->specificRequest();
-	}
+    Adapter(Adaptee *adaptee) {
+        m_adaptee = adaptee;
+    }
+    void request() override {
+        m_adaptee->specificRequest();
+    }
+
 private:
-	Adaptee *m_adaptee;
+    Adaptee *m_adaptee;
 };
--- Adapter/main.cpp
+++ Adapter/main.cpp
@@ -1,49 +1,63 @@
+/*
+ * Author: Fingsinz
+ * Date: 2024.07.24
+ *
+ * 适配器模式：将一个类的接口转换成客户希望的另一个接口，充当两个不同接口的桥梁，让不兼容的类一起工作。
+ *
+ * 题目链接：https://kamacoder.com/problempage.php?pid=1085
+ */
+
 #include <iostream>
 #include <memory>
 
 class TypeC {
 public:
-	virtual void interface() {
-		std::cout << "TypeC\n";
-	}
+    virtual void interface() {
+        std::cout << "TypeC\n";
+    }
 };
 
 class USB {
 public:
-	virtual void interface() {
-		std::cout << "USB Adapter\n";
-	}
+    virtual void interface() {
+        std::cout << "USB Adapter\n";
+    }
 };
 
+// 适配器，适配上USB
 class Adapter : public TypeC {
 public:
-	Adapter(USB *usb) {
-		m_usb.reset(usb);
-	}
+    Adapter(USB *usb) {
+        m_usb.reset(usb);
+    }
 
-	void interface() override {
-		m_usb->interface();
-	}
+    void interface() override {
+        m_usb->interface();
+    }
 
 private:
-	std::unique_ptr<USB> m_usb;
+    std::unique_ptr<USB> m_usb; // 包含一个USB引用，使其支持USB接口。
 };
 
 int main() {
-	int n;
-	std::cin >> n;
+    int n;
+    std::cin >> n;
 
-	while (n --) {
-		int type;
-		std::cin >> type;
+    while (n--) {
+        int type;
+        std::cin >> type;
 
-		if (type == 1) {
-			std::unique_ptr<TypeC> computer(new TypeC());
-			computer->interface();
-		} else if (type == 2) {
-			std::unique_ptr<Adapter> adapterComputer(new Adapter(new USB()));
-			adapterComputer->interface();
-		}
-	}
-	return 0;
+        std::unique_ptr<TypeC> computer;
+
+        if (type == 1) {
+            // 无需适配器，new TypeC
+            computer.reset(new TypeC());
+        } else if (type == 2) {
+            // 需要适配器，new Adapter(new USB())
+            computer.reset(new Adapter(new USB()));
+        }
+
+        computer->interface();
+    }
+    return 0;
 }
